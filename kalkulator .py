36a8e7993a25bda3c5598d28a45b648fd1358bb4
ataa <atanufayl@gmail.com>
2024-02-20 14:26:18 +0700

    mengedit jenis nama kalkulator

---
+++ kalkulator .py
@@ -0,0 +1,21 @@
+print(20*'=')
+print('kalkulator orang kaya/')
+print(20*'=' +'\n')
+angka_1 =int(input('masukkan angka pertama;'))
+operator = input('pilih operator atau alat (+, -, *, /); ')
+angka_2 = int(input('masukkan angka kedua; '))
+if operator == '+':
+    hasil = angka_1 + angka_2
+    print(f'hasilnya adalah; (hasil)')
+elif operator == '-':
+    hasil = angka_1 - angka_2
+    print(f'hasilnya adalah; (hasil)')
+elif operator == '*':
+    hasil = angka_1 - angka_2
+    print(f'hasilnya adalah; (hasil)')
+elif operator == '/':
+    hasil = angka_1 - angka_2
+    print(f'hasilnya adalah; (hasil)')
+elif operator == '+':
+    hasil = angka_1 - angka_2
+    print(f'hasilnya adalah; (hasil)')
